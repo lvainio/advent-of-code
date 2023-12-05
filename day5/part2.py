4f76156b8c7fa7e6b9212d7429a177f0f544149c
lvainio <lvainio@kth.se>
2023-12-05 14:58:11 +0100

    solution part 2 day 5

--- day5/part2.py
+++ day5/part2.py
@@ -1,45 +1,74 @@
 
-import time
+def parse_seed_data(data):
+    """Parse first line of input and return a list of (start, length) tuples"""
 
-start_time = time.time()
+    seeds = [int(i) for i in data.split()[1:]]
+    return list(zip(seeds[::2], seeds[1::2]))
 
-data = open("input.txt").read().split("\n\n")
+def parse_mappings(data):
+    """Parse the mappings and return a list of all mappings"""
 
-seeds = [int(i) for i in data[0].split()[1:]]
+    mappings= []
+    for item in data:
+        mappings.append([list(map(int, line.split())) for line in item.split("\n")[1:]])
+    return mappings
 
-maps= []
-for item in data[1:8]:
-    maps.append([list(map(int, line.split())) for line in item.split("\n")[1:]])
+with open("input.txt") as file:
+    data = file.read().split("\n\n")
+
+seed_ranges = parse_seed_data(data[0])
+mappings = parse_mappings(data[1:])
+
+range_tuples = [seed_ranges, [],[],[],[],[],[],[]]
+
+for i, mapping in enumerate(mappings):
+    # Sort mapping in ascending order based on source value.
+    sorted_mapping = sorted(mapping, key=lambda x: x[1])
+
+    for start, length in range_tuples[i]:  
+        # Keep track of where to start the next range
+        range_start = start 
+
+        for dest, source, r in sorted_mapping:
+            # Mapping range is to the left of this tuple.
+            if source+r-1 < start: 
+                continue
+            # Mapping range is to the right of this tuple.
+            if source >= start+length:
+                break
+
+            tuple_start = dest
+            tuple_range = r
+
+            # Mapping range starts before the tuple's range.
+            if source < start:
+                tuple_start = dest + start - source
+                tuple_range -= start-source
+
+            # Mapping range is to the right of where next range should start so we need to 
+            # insert a tuple that has no mapping.
+            if source > range_start:
+                range_tuples[i+1].append((range_start, source-range_start))
+
+            # Mapping ends within the range of our tuple's range.
+            if source + r <= start + length:
+                range_tuples[i+1].append((tuple_start, tuple_range))
+                range_start = source+r
+
+            # Mapping ends outside of our tuple's range.
+            else:
+                tuple_range -= (source+r) - (start+length)
+                range_tuples[i+1].append((tuple_start, tuple_range))
+                range_start = start+length # Set start of next range beyond the limit 
+                break
+
+        # If mappings have not covered all range we need to add a tuple to cover the last 
+        # bit of our range.
+        if range_start < start+length:
+            range_tuples[i+1].append((range_start, start+length-range_start))
 
 min_location = float('inf')
-
-counter = 0
-
-for i in range(0, len(seeds), 2): 
-
-    for seed in range(seeds[i], seeds[i]+seeds[i+1]):
-
-        counter += 1
-        if counter % 1_000_000 == 0:
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print(f"Elapsed time: {elapsed_time} seconds")
-            print("hi")
-
-        number = seed
-        for i in range(len(maps)): 
-            for j in range(len(maps[i])):
-                [dest, src, r] = maps[i][j]
-                if src <= number < src + r: 
-                    number = dest + (number - src) 
-                    break
-
-        if number < min_location:
-            min_location = number
-
-    break
+for tuple in range_tuples[7]:
+    min_location = min(min_location, tuple[0])
 
 print(min_location)
-            
-# would take around 8 hours to run this program
-# so another approach is needed lol

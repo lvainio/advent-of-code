901f98bbb1dcb3a6760d23fa7701d80fb6de618a
lvainio <lvainio@kth.se>
2023-12-05 08:41:07 +0100

    added naive

--- day5/part2.py
+++ day5/part2.py
@@ -1,4 +1,8 @@
 
+import time
+
+start_time = time.time()
+
 data = open("input.txt").read().split("\n\n")
 
 seeds = [int(i) for i in data[0].split()[1:]]
@@ -8,9 +12,19 @@ for item in data[1:8]:
     maps.append([list(map(int, line.split())) for line in item.split("\n")[1:]])
 
 min_location = float('inf')
+
+counter = 0
+
 for i in range(0, len(seeds), 2): 
 
     for seed in range(seeds[i], seeds[i]+seeds[i+1]):
+
+        counter += 1
+        if counter % 1_000_000 == 0:
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Elapsed time: {elapsed_time} seconds")
+            print("hi")
 
         number = seed
         for i in range(len(maps)): 
@@ -23,5 +37,9 @@ for i in range(0, len(seeds), 2):
         if number < min_location:
             min_location = number
 
+    break
+
 print(min_location)
             
+# would take around 8 hours to run this program
+# so another approach is needed lol

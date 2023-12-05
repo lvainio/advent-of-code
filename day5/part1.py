d27224f4ad02c18e0af8da8d729b833b3df720db
lvainio <lvainio@kth.se>
2023-12-05 08:27:10 +0100

    solution day 5 part 1

---
+++ day5/part1.py
@@ -0,0 +1,23 @@
+
+data = open("input.txt").read().split("\n\n")
+
+seeds = [int(i) for i in data[0].split()[1:]]
+
+maps= []
+for item in data[1:8]:
+    maps.append([list(map(int, line.split())) for line in item.split("\n")[1:]])
+
+min_location = float('inf')
+for seed in seeds: 
+    number = seed
+    for i in range(len(maps)): 
+        for j in range(len(maps[i])):
+            [dest, src, r] = maps[i][j]
+            if src <= number < src + r: 
+                number = dest + (number - src) 
+                break
+
+    if number < min_location:
+        min_location = number
+
+print(min_location)

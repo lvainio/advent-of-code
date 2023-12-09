c385c022e343bf62045eca1597a6df80ee52d155
lvainio <leojvainio@gmail.com>
2023-12-09 09:14:00 +0100

    solution day 9

---
+++ day9/part2.py
@@ -0,0 +1,19 @@
+
+with open('input.txt') as file:
+    total = 0
+    for line in file.read().splitlines():
+        numbers = [int(x) for x in line.split()]
+        differences = [numbers]
+
+        for i in range(len(numbers)):
+            diff = [n2 - n1 for n1, n2 in zip(differences[i][:-1], differences[i][1:])]
+            if all(x == 0 for x in diff):
+                break
+            differences.append(diff)
+
+        sum = 0
+        for i in range(len(differences) - 1, -1, -1):
+            sum = differences[i][0] - sum
+        total += sum
+
+    print(total)

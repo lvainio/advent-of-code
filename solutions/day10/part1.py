ee618292c6b9a4664ff5b61fdbdf5122ce762924
lvainio <leojvainio@gmail.com>
2023-12-10 13:06:08 +0100

    solution day 10 part1

---
+++ solutions/day10/part1.py
@@ -0,0 +1,76 @@
+
+# index (i, j) of S in grid
+def find_start_position(grid):
+    for i, row in enumerate(grid):
+        try:
+            j = row.index('S')
+            return (i, j)
+        except ValueError:
+            continue
+    return None 
+
+# checks whether indexes are within the grid.
+def is_within_grid(grid, position):
+    i, j = position
+    return 0 <= i < len(grid) and 0 <= j < len(grid[0])
+
+# list of (i, j) of all neighbors of node
+def get_neighbors(grid, node):
+    i, j = node
+    symbol = grid[i][j]
+
+    directions = {
+        'S': [(i-1, j), (i+1, j), (i, j-1), (i, j+1)],  # North, South, West, East
+        '|': [(i-1, j), (i+1, j)],                      # North, South
+        '-': [(i, j-1), (i, j+1)],                      # West, East
+        'L': [(i-1, j), (i, j+1)],                      # North, East
+        'J': [(i-1, j), (i, j-1)],                      # North, West
+        '7': [(i, j-1), (i+1, j)],                      # West, South
+        'F': [(i, j+1), (i+1, j)],                      # East, South
+        '.': []
+    }
+
+    neighbors = []
+    for direction in directions.get(symbol, []):
+        if is_within_grid(grid, direction):
+            neighbors.append(direction)
+
+    return neighbors
+
+# find length of the cycle my man
+def get_cycle_length(grid, start):
+    visited = set()
+    stack = [(start, 0)]
+
+    while stack:
+        node, length = stack.pop()
+
+        if node in visited:
+            if node == start and length > 1:
+                return length
+            continue
+
+        visited.add(node)
+        for neighbor in get_neighbors(grid, node):
+            if neighbor not in visited or (neighbor == start and length > 1):
+                stack.append((neighbor, length + 1))
+
+    return 0
+            
+def main():
+    with open('input.txt') as file:
+        grid = file.read().splitlines()
+
+        start = find_start_position(grid)
+
+        length = get_cycle_length(grid, start)
+
+        print(length // 2)
+
+
+
+if __name__ == "__main__": 
+    main()
+
+
+    

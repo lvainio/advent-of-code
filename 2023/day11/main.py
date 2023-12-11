801b4a1564070e99a1c743071fdc3599e663b7a1
lvainio <leojvainio@gmail.com>
2023-12-11 10:15:45 +0100

    solution day11

--- 2023/day11/main.py
+++ 2023/day11/main.py
@@ -1,56 +1,44 @@
 import itertools
 
-def expand(universe):
-    expanded_rows = [line for line in universe for _ in (range(2) if '#' not in line else range(1))]
-    expanded_cols = [""] * len(expanded_rows)
+def distance(n, g1, g2, empty_rows, empty_cols):
+    distance = abs(g2[0] - g1[0]) + abs(g2[1] - g1[1])
 
-    num_rows = len(expanded_rows)
-    num_cols = len(expanded_rows[0])
-    for col in range(num_cols):
-        contains_galaxy = any('#' in expanded_rows[row][col] for row in range(num_rows))
-        for row in range(num_rows):
-            if not contains_galaxy:
-                expanded_cols[row] += expanded_rows[row][col] * 2
-            else:
-                expanded_cols[row] += expanded_rows[row][col]
-            
-    return expanded_cols
+    for row_index in range(min(g1[0], g2[0])+1, max(g1[0], g2[0])):
+        if row_index in empty_rows:
+            distance += n-1
 
-def manhattan_distance(g1, g2):
-    return abs(g2[0] - g1[0]) + abs(g2[1] - g1[1])
+    for col_index in range(min(g1[1], g2[1])+1, max(g1[1], g2[1])):
+        if col_index in empty_cols:
+            distance += n-1
+
+    return distance
 
 def main():
     with open("input.txt") as file:
         universe = file.read().splitlines()
 
-    expanded = expand(universe)
-    num_rows = len(expanded)
-    num_cols = len(expanded[0])
+    num_rows = len(universe)
+    num_cols = len(universe[0])
 
     galaxies = []
     for row, col in itertools.product(range(num_rows), range(num_cols)):
-        if expanded[row][col] == '#':
+        if universe[row][col] == '#':
             galaxies.append((row, col))
 
-    sum = 0
-    for g1, g2 in itertools.combinations(galaxies, 2):
-        sum += manhattan_distance(g1, g2)
-    print(sum)
-        
-
- 
+    empty_rows = [i for i, row in enumerate(universe) if '#' not in row]
+    empty_cols = []
+    for col in range(num_cols):
+        contains_galaxy = any('#' in universe[row][col] for row in range(num_rows))
+        if not contains_galaxy:
+            empty_cols.append(col)
     
-
-
+    part1, part2 = 0, 0
+    for g1, g2 in itertools.combinations(galaxies, 2):
+        part1 += distance(2, g1, g2, empty_rows, empty_cols)
+        part2 += distance(1_000_000, g1, g2, empty_rows, empty_cols)
+    print(f'part1: {part1}, part2: {part2}')
         
 
-
-                
-
-
-
-
-
-
+        
 if __name__ == '__main__':
     main()

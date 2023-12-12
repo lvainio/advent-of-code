f49d0f258456458ac910abbedc7ceca87692f2ed
lvainio <leojvainio@gmail.com>
2023-12-12 15:46:42 +0100

    solution day 12

--- 2020/day4/main.c
+++ 2020/day4/main.c
@@ -2,9 +2,14 @@
 
 int main(int argc, char **argv) {
 
-    printf("hi %d", 4);
+    FILE *file;
+    file = fopen("input.txt", "r");
 
+    char *text;
+
+    while(fgets(text, 140, file) != NULL) { // newline included in string, remember that when checking for len
+        printf("%s", text);
+    }
 
     return 0;
-
 }
--- 2023/day12/main.py
+++ 2023/day12/main.py
@@ -1,27 +1,42 @@
-import re
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None) 
+def count(record, sizes):
+    if sizes == () and not '#' in record:
+        return 1
+    elif sizes == () or record == '':
+        return 0
+
+    total = 0
+    if record[0] in '.?': # Treat as '.'
+        total += count(record[1:], sizes)
+
+    if record[0] in "#?": # Treat as '#'
+        fits_in_record = sizes[0] <= len(record)
+        contains_no_dots = "." not in record[:sizes[0]]
+
+        if fits_in_record and contains_no_dots:
+            valid_next_char = (sizes[0] == len(record) or record[sizes[0]] != "#")
+
+            if valid_next_char:
+                total += count(record[sizes[0]+1:], sizes[1:])
+
+    return total
+
 
 def main():
     with open('input.txt') as file:
-        lines = file.read().splitlines()
-
-        for line in lines:
+        part1 = 0
+        part2 = 0
+        for line in file.read().splitlines():
             record, sizes = line.split()
+            sizes = tuple(int(x) for x in sizes.split(','))
+            
+            part1 += count(record, sizes)
+            part2 += count('?'.join([record]*5), sizes*5)
 
-            springs = re.findall(r'[#?]+', record)
-            sizes = [int(x) for x in sizes.split(',')]
-
-            # brute force is one way ofc but seems annoying
-
-            # '#' must be spring right, '?' can be either. 
-
-            # can there be 0 arrangements?
-
-            # trying possible arrangements of ?
-
-
-
-
-
+        print(f'part1: {part1}, part2: {part2}')
 
 
 if __name__ == '__main__':

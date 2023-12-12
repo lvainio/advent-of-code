13c768504dbfcba9cf9cd9f5930dd7762cd66d4c
lvainio <leojvainio@gmail.com>
2023-12-12 20:40:44 +0100

    solution day 4 2020

--- 2020/day4/main.c
+++ 2020/day4/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
 char *fields[] = { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid" };
 char *optional[] = { "cid" };
-
-int seen_fields[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
 int main(int argc, char **argv) {
 
@@ -13,17 +12,24 @@ int main(int argc, char **argv) {
     char *text;
     int total = 0;
 
-    while(fgets(text, 140, file) != NULL) { // newline included in string, remember that when checking for len
+    int num_fields_seen = 0;
+    while(fgets(text, 140, file) != NULL) { // newline included in string, remember that when checking for len        
         if (strcmp(text,"\n") == 0) { // blank line means new passport
-            printf("blank line");
-            // check if all seen fields excep cid = 1
-            // if true then increment total
-            // reset all seen fields to 0
-            // continue;
+            if (num_fields_seen == 7) { // all except cid we dont care about cid. 
+                total++;
+            }
+            num_fields_seen = 0;
+            continue;
         }
 
-        // process a line of fields. 
-        // increment seen_fields if we see a field for each field in line. 
+        char *token = strtok(text, " ");
+        while (token != NULL) {
+            token = strtok(NULL, delimiters);
+            char *t2 = strtok(token, ":");
+            if strcmp(t2, "cid") {
+                num_fields_seen++;
+            }
+        }
     }
 
     printf("%d", total);

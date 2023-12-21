d99ccb36c61997bac69d4048cf87f388a1b9218d
lvainio <leojvainio@gmail.com>
2023-12-21 13:18:09 +0100

    tweak

--- 2023/day21/main.py
+++ 2023/day21/main.py
@@ -1,17 +1,20 @@
 from collections import deque
 from itertools import product
 
+
 def find_start(grid):
     for row, col in product(range(len(grid)), range(len(grid[0]))):
         if grid[row][col] == 'S':
             return (row, col)
         
+
 def is_valid(grid, row, col):
     return 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] != '#'
 
+
 def part1(grid, num_steps, start_row, start_col):
+    seen = set()
     queue = deque([(start_row, start_col, 0)])
-    visited = set()
     while queue:
         if queue[0][2] == num_steps:
             return len(queue)
@@ -19,9 +22,9 @@ def part1(grid, num_steps, start_row, start_col):
         for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]: 
             nr = row + dr
             nc = col + dc
-            if is_valid(grid, nr, nc) and not (nr, nc, level+1) in visited:
+            if is_valid(grid, nr, nc) and not (nr, nc, level+1) in seen:
                 queue.append((nr, nc, level+1))
-                visited.add((nr, nc, level+1))
+                seen.add((nr, nc, level+1))
 
 
 def part2(grid, num_steps):

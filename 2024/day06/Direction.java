28613e9cca9cf7bb04f4000554dbc7bd92a20a30
lvainio <lvainio@kth.se>
2025-01-07 15:58:25 +0100

    Add day 6 2024

--- 2024/day06/Direction.java
+++ 2024/day06/Direction.java
@@ -1,8 +1,8 @@
 public enum Direction {
-    UP(0, -1),   
-    DOWN(0, 1),  
-    LEFT(-1, 0), 
-    RIGHT(1, 0); 
+    UP(-1, 0),   
+    DOWN(1, 0),  
+    LEFT(0, -1), 
+    RIGHT(0, 1); 
 
     private final int dr; 
     private final int dc; 
--- 2024/day06/Part2.java
+++ 2024/day06/Part2.java
@@ -1,15 +1,20 @@
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Part2 {
     private final char OBSTACLE = '#';
     private final char FREE_SPACE = '.';
     private final char STARTING_POSITION = '^';
 
-    private char[][] grid;
+    private final char[][] grid;
 
     private Direction currentDirection;
 
     private int currentRow;
     private int currentCol;
+
+    private int startingRow;
+    private int startingCol;
 
     public Part2(char[][] grid) {
         // Copy grid
@@ -23,6 +28,8 @@ public class Part2 {
                 if (grid[row][col] == STARTING_POSITION) {
                     this.currentRow = row;
                     this.currentCol = col;
+                    this.startingRow = row;
+                    this.startingCol = col;
                 }
             }
         }
@@ -33,8 +40,57 @@ public class Part2 {
     }
 
     public void solve() {
-        int sum = 0;
+        int count = 0;
+        for (int row = 0; row < this.grid.length; row++) {
+            for (int col = 0; col < this.grid[0].length; col++) {
+                if (this.grid[row][col] == this.FREE_SPACE && !(row == this.startingRow && col == this.startingCol)) {
+                    HashMap<Integer, HashMap<Integer, HashSet<Direction>>> visited = new HashMap<>();
+                    this.grid[row][col] = this.OBSTACLE;
+                    while (!isOutsideGrid()) {
+                        if (visited.get(this.currentRow) != null && 
+                            visited.get(this.currentRow).get(this.currentCol) != null &&
+                            visited.get(this.currentRow).get(this.currentCol).contains(this.currentDirection)) 
+                        {   
+                            count++;
+                            break;
+                        }
+                        visited
+                            .computeIfAbsent(this.currentRow, k -> new HashMap<>())  
+                            .computeIfAbsent(this.currentCol, k -> new HashSet<>())
+                            .add(this.currentDirection);
+                        step();
+                    }
+                    this.grid[row][col] = FREE_SPACE;
+                    this.currentRow = this.startingRow;
+                    this.currentCol = this.startingCol;
+                    this.currentDirection = Direction.UP;
+                }
+            }
+        }
+        System.out.println("Part2: " + count);
+    }
 
-        System.out.println("Part2: " + sum);
+    private boolean isOutsideGrid() {
+        return this.currentRow < 0 || this.currentRow >= this.grid.length ||
+                this.currentCol < 0 || this.currentCol >= this.grid[0].length;
+    }
+
+    private void step() {
+        while (nextIsObstacle()) {
+            this.currentDirection = this.currentDirection.turnRight();
+        }
+
+        this.currentRow = this.currentRow + this.currentDirection.getDr();
+        this.currentCol = this.currentCol + this.currentDirection.getDc();
+    }
+
+    private boolean nextIsObstacle() {
+        int newRow = this.currentRow + this.currentDirection.getDr();
+        int newCol = this.currentCol + this.currentDirection.getDc();
+        if (newRow < 0 || newRow >= this.grid.length ||
+            newCol < 0 || newCol >= this.grid[0].length) {
+                return false;
+        }
+        return this.grid[newRow][newCol] == OBSTACLE;
     }
 }

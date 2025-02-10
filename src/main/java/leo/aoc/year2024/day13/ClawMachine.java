abb02288959f3a4309da4ef107b548b64bbc65ab
lvainio <lvainio@kth.se>
2025-02-10 17:31:58 +0100

    wip

--- src/main/java/leo/aoc/year2024/day13/ClawMachine.java
+++ src/main/java/leo/aoc/year2024/day13/ClawMachine.java
@@ -12,7 +12,6 @@ public record ClawMachine(
         double aPresses = (double) (this.by * this.px - this.bx * this.py) / 
                             (this.ax * this.by - this.ay * this.bx);
         double bPresses = (double) (this.px - aPresses * this.ax) / this.bx;
-
         if (aPresses >= 0.0 && bPresses >= 0.0 && aPresses % 1.0 == 0.0 && bPresses % 1 == 0.0) {
             return (long) (aPresses * 3.0 + bPresses);
         }
--- src/main/java/leo/aoc/year2024/day13/Solver.java
+++ src/main/java/leo/aoc/year2024/day13/Solver.java
@@ -19,19 +19,15 @@ public class Solver extends AbstractSolver {
         List<ClawMachine> clawMachines = IntStream.iterate(0, idx -> idx < lines.size(), idx -> idx + 4)
             .mapToObj(idx -> {
                 Pattern pattern = Pattern.compile("\\d+");
-
                 Matcher matcherButtonA = pattern.matcher(lines.get(idx));
                 int ax = this.getNext(matcherButtonA);
                 int ay = this.getNext(matcherButtonA);
-
                 Matcher matcherButtonB = pattern.matcher(lines.get(idx+1));
                 int bx = this.getNext(matcherButtonB);
                 int by = this.getNext(matcherButtonB);
-
                 Matcher matcherPrice = pattern.matcher(lines.get(idx+2));
                 int px = this.getNext(matcherPrice);
                 int py = this.getNext(matcherPrice);
-
                 return new ClawMachine(ax, ay, bx, by, px, py);
             }).collect(Collectors.toList());
         this.clawMachines = clawMachines;

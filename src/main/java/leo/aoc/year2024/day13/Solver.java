0b5c4d9d6056fde1fbe1360ca8e2ea19cef0f1c2
lvainio <lvainio@kth.se>
2025-02-10 17:36:03 +0100

    wip

--- src/main/java/leo/aoc/year2024/day13/Solver.java
+++ src/main/java/leo/aoc/year2024/day13/Solver.java
@@ -10,6 +10,35 @@ import leo.aoc.AbstractSolver;
 
 public class Solver extends AbstractSolver {
 
+    private record ClawMachine(
+        long ax,
+        long ay, 
+        long bx, 
+        long by, 
+        long px,
+        long py
+    ) {
+        public long calculateCheapestWin() {
+            double aPresses = (double) (this.by * this.px - this.bx * this.py) / 
+                                (this.ax * this.by - this.ay * this.bx);
+            double bPresses = (double) (this.px - aPresses * this.ax) / this.bx;
+            if (aPresses >= 0.0 && bPresses >= 0.0 && aPresses % 1.0 == 0.0 && bPresses % 1 == 0.0) {
+                return (long) (aPresses * 3.0 + bPresses);
+            }
+            return 0;
+        }
+
+        public ClawMachine addLongToPrizeCoordinates(long num) {
+            return new ClawMachine(
+                this.ax, 
+                this.ay, 
+                this.bx, 
+                this.by, 
+                this.px + num, 
+                this.py + num);
+        }
+    }
+
     private final List<ClawMachine> clawMachines;
 
     public Solver(String input) {

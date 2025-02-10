e6ccdaecd7b497429bb250aac191c705b8ff9d34
lvainio <lvainio@kth.se>
2025-02-10 16:50:08 +0100

    Add day8 2024

--- 2024/day08/src/main/java/com/example/day08_2024/Node.java
+++
@@ -1,9 +0,0 @@
-package com.example.day08_2024;
-
-public record Node(int row, int col) {
-
-    public boolean isInsideGrid(int numRows, int numCols) {
-        return !(row < 0 || row >= numRows ||
-                col < 0 || col >= numCols);
-    }
-}

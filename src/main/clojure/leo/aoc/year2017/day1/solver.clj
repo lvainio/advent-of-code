3e3472bcf964df039d290ba21d449c5979f2fe30
lvainio <lvainio@kth.se>
2025-02-16 12:49:26 +0100

    Add day1 2017

--- src/main/clojure/leo/aoc/year2017/day1/solver.clj
+++ src/main/clojure/leo/aoc/year2017/day1/solver.clj
@@ -1,7 +1,21 @@
 (ns leo.aoc.year2017.day1.solver)
 
 (defn solve-part1 [input]
-  (str "Solved Part 1 in Clojure with input: " (+ 5 6)))
+  (let [firstDigits (seq input)
+        secondDigits (conj (vec (rest input)) (last input))]
+    (->> (map vector firstDigits secondDigits)
+         (filter #(= (first %) (second %)))
+         (map #(Character/digit (first %) 10))
+         (reduce +)
+         (str))))
 
 (defn solve-part2 [input]
-  (str "Solved Part 2 in Clojure with input: " (- 10 5)))
+  (let [firstDigits  (seq input)  
+        size         (count firstDigits)
+        halfSize     (/ size 2)
+        secondDigits (map #(nth input (mod (+ % halfSize) size)) (range size))] 
+    (->> (map vector firstDigits secondDigits)  
+         (filter #(= (first %) (second %))) 
+         (map #(Character/digit (first %) 10))  
+         (reduce +)  
+         (str)))) 

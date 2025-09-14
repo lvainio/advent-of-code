08b21fbb68a7522e5974322b092a40cf0037f63e
lvainio <61742173+lvainio@users.noreply.github.com>
2025-09-14 08:39:24 +0200

    Leo/bazel version (#4)
    * Pin bazel version to 8.4.1
    
    * Add initial bazel setup

---
+++ core/Utils.java
@@ -0,0 +1,7 @@
+package core;
+
+public class Utils {
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import core.Utils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utils.greet("Advent of Code 2025!"));
+    }
+}

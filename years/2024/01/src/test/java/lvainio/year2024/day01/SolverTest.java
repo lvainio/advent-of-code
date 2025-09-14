ff4b0698e9d3bcecce76917e4ab9b12fabc255f0
lvainio <leojvainio@gmail.com>
2025-09-14 14:14:29 +0200

    Add Junit5 support

---
+++ years/2024/01/src/test/java/lvainio/year2024/day01/SolverTest.java
@@ -0,0 +1,19 @@
+package lvainio.year2024.day01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class SolverTest {
+
+    @Test
+    void testSolvePart1() {
+        String result = "3";
+        assertEquals("3", result);
+    }
+
+    @Test
+    void testSolvePart2() {
+        String result = "3";
+        assertEquals("3", result);
+    }
+}

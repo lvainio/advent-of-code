f1ab3a089378fc6479ab6fe1d863c8ceb106ba45
lvainio <leojvainio@gmail.com>
2025-12-07 19:29:17 +0100

    2024 day 4

---
+++ solutions/2024/04/src/test/java/me/vainio/aoc/year2024/day04/SolverTest.java
@@ -0,0 +1,38 @@
+package me.vainio.aoc.year2024.day04;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SolverTest {
+
+    private static final String EXAMPLE_INPUT = """
+            MMMSXXMASM
+            MSAMXMSMSA
+            AMXSXMAAMM
+            MSAMASMSMX
+            XMASAMXAMM
+            XXAMMXXAMA
+            SMSMSASXSS
+            SAXAMASAAA
+            MAMMMXMMMM
+            MXMXAXMASX""";
+
+    private static Solver solver;
+
+    @BeforeAll
+    static void setUp() {
+        solver = new Solver(EXAMPLE_INPUT);
+    }
+
+    @Test
+    void testSolvePart1() {
+        assertEquals("18", solver.solvePart1());
+    }
+
+    @Test
+    void testSolvePart2() {
+        assertEquals("9", solver.solvePart2());
+    }
+}

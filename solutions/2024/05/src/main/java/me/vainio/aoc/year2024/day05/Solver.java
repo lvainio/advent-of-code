178829402be869c01e9f7eec3a9b1bef9a1e58df
lvainio <leojvainio@gmail.com>
2025-12-15 13:11:36 +0100

    Move 2024 5 to new format

---
+++ solutions/2024/05/src/main/java/me/vainio/aoc/year2024/day05/Solver.java
@@ -0,0 +1,110 @@
+package me.vainio.aoc.year2024.day05;
+
+import me.vainio.aoc.cache.AocCache;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Solver {
+    private static final int YEAR = 2024;
+    private static final int DAY = 5;
+
+    private final HashMap<Integer, HashSet<Integer>> orderingRulesMap;
+    private final List<List<Integer>> updates;
+
+    public static void main(final String[] args) {
+        final AocCache cache = new AocCache();
+
+        final String input = cache.getInput(YEAR, DAY);
+        final Solver solver = new Solver(input);
+
+        final String part1 = solver.solvePart1();
+        final String part2 = solver.solvePart2();
+
+        System.out.println(part1);
+        System.out.println(part2);
+
+        cache.saveAnswer(YEAR, DAY, 1, part1);
+        cache.saveAnswer(YEAR, DAY, 2, part2);
+    }
+
+    public Solver(final String input) {
+        final String[] content = input.split("\r?\n\r?\n");
+
+        final List<List<Integer>> orderingRules =
+                Arrays.stream(content[0].split("\r?\n"))
+                        .map(
+                                line ->
+                                        Arrays.stream(line.split("\\|"))
+                                                .map(Integer::parseInt)
+                                                .collect(Collectors.toList()))
+                        .toList();
+
+        this.orderingRulesMap = new HashMap<>();
+        for (List<Integer> tuple : orderingRules) {
+            int x = tuple.get(0);
+            int y = tuple.get(1);
+            orderingRulesMap.computeIfAbsent(x, k -> new HashSet<>()).add(y);
+        }
+
+        this.updates =
+                Arrays.stream(content[1].split("\r?\n"))
+                        .map(
+                                line ->
+                                        Arrays.stream(line.split(","))
+                                                .map(Integer::parseInt)
+                                                .collect(Collectors.toList()))
+                        .collect(Collectors.toList());
+    }
+
+    public String solvePart1() {
+        int sum =
+                this.updates.stream()
+                        .filter(this::isUpdateCorrectlyOrdered)
+                        .map(update -> update.get(update.size() / 2))
+                        .reduce(0, Integer::sum);
+        return String.valueOf(sum);
+    }
+
+    public String solvePart2() {
+        int sum =
+                this.updates.stream()
+                        .filter(update -> !isUpdateCorrectlyOrdered(update))
+                        .map(this::orderUpdate)
+                        .map(update -> update.get(update.size() / 2))
+                        .reduce(0, Integer::sum);
+        return String.valueOf(sum);
+    }
+
+    private boolean isUpdateCorrectlyOrdered(List<Integer> update) {
+        for (int i = 0; i < update.size(); i++) {
+            for (int j = i + 1; j < update.size(); j++) {
+                Set<Integer> rulesForJ = this.orderingRulesMap.get(update.get(j));
+                if (rulesForJ != null && rulesForJ.contains(update.get(i))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> orderUpdate(List<Integer> update) {
+        List<Integer> updateOrdered = new ArrayList<>(update);
+        updateOrdered.sort(
+                (num1, num2) -> {
+                    if (orderingRulesMap.get(num1) != null && orderingRulesMap.get(num1).contains(num2)) {
+                        return -1;
+                    } else if (orderingRulesMap.get(num2) != null
+                            && orderingRulesMap.get(num2).contains(num1)) {
+                        return 1;
+                    }
+                    return 0;
+                });
+        return updateOrdered;
+    }
+}
---
+++ solutions/2024/05/src/test/java/me/vainio/aoc/year2024/day05/SolverTest.java
@@ -0,0 +1,56 @@
+package me.vainio.aoc.year2024.day05;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SolverTest {
+
+    private static final String EXAMPLE_INPUT = """
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+            
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47""";
+
+    private static Solver solver;
+
+    @BeforeAll
+    static void setUp() {
+        solver = new Solver(EXAMPLE_INPUT);
+    }
+
+    @Test
+    void testSolvePart1() {
+        assertEquals("143", solver.solvePart1());
+    }
+
+    @Test
+    void testSolvePart2() {
+        assertEquals("123", solver.solvePart2());
+    }
+}

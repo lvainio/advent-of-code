b685fed11de9e0ce6c088c4289804fa8013d48af
lvainio <leojvainio@gmail.com>
2025-12-18 18:15:24 +0100

    Port day 8 2024

--- solutions/2024/08/src/main/java/me/vainio/aoc/year2024/day08/Solver.java
+++ solutions/2024/08/src/main/java/me/vainio/aoc/year2024/day08/Solver.java
@@ -1,12 +1,14 @@
-package leo.aoc.year2024.day8;
+package me.vainio.aoc.year2024.day08;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import leo.aoc.AbstractSolver;
+import me.vainio.aoc.cache.AocCache;
 
-public class Solver extends AbstractSolver {
+public class Solver {
+  private static final int YEAR = 2024;
+  private static final int DAY = 8;
 
   private static final char EMPTY_SPACE = '.';
 
@@ -20,37 +22,50 @@ public class Solver extends AbstractSolver {
   private final int numCols;
   private final HashMap<Character, List<Node>> antennas;
 
-  public Solver(String input) {
-    super(input);
+  public static void main(final String[] args) {
+    final AocCache cache = new AocCache();
 
-    char[][] grid = input.lines().map(String::toCharArray).toArray(char[][]::new);
+    final String input = cache.getInput(YEAR, DAY);
+    final Solver solver = new Solver(input);
+
+    final String part1 = solver.solvePart1();
+    final String part2 = solver.solvePart2();
+
+    System.out.println(part1);
+    System.out.println(part2);
+
+    cache.saveAnswer(YEAR, DAY, 1, part1);
+    cache.saveAnswer(YEAR, DAY, 2, part2);
+  }
+
+  public Solver(final String input) {
+    final char[][] grid = input.lines().map(String::toCharArray).toArray(char[][]::new);
     this.numRows = grid.length;
     this.numCols = grid[0].length;
 
-    HashMap<Character, List<Node>> antennas = new HashMap<>();
+    final HashMap<Character, List<Node>> antennas = new HashMap<>();
     for (int row = 0; row < grid.length; row++) {
       for (int col = 0; col < grid[0].length; col++) {
         if (grid[row][col] != EMPTY_SPACE) {
-          antennas.computeIfAbsent(grid[row][col], _ -> new ArrayList<>()).add(new Node(row, col));
+          antennas.computeIfAbsent(grid[row][col], k -> new ArrayList<>()).add(new Node(row, col));
         }
       }
     }
     this.antennas = antennas;
   }
 
-  @Override
   public String solvePart1() {
-    HashSet<Node> antiNodesPart = new HashSet<>();
+    final HashSet<Node> antiNodesPart = new HashSet<>();
     for (List<Node> nodes : this.antennas.values()) {
       for (int i = 0; i < nodes.size(); i++) {
         for (int j = i + 1; j < nodes.size(); j++) {
-          Node antenna1 = nodes.get(i);
-          Node antenna2 = nodes.get(j);
-          Node antiNode1 =
+          final Node antenna1 = nodes.get(i);
+          final Node antenna2 = nodes.get(j);
+          final Node antiNode1 =
               new Node(
                   antenna1.row() + (antenna1.row() - antenna2.row()),
                   antenna1.col() + (antenna1.col() - antenna2.col()));
-          Node antiNode2 =
+          final Node antiNode2 =
               new Node(
                   antenna2.row() + (antenna2.row() - antenna1.row()),
                   antenna2.col() + (antenna2.col() - antenna1.col()));
@@ -66,20 +81,19 @@ public class Solver extends AbstractSolver {
     return Integer.toString(antiNodesPart.size());
   }
 
-  @Override
   public String solvePart2() {
-    HashSet<Node> antiNodesPart = new HashSet<>();
+    final HashSet<Node> antiNodesPart = new HashSet<>();
     for (List<Node> nodes : this.antennas.values()) {
       for (int i = 0; i < nodes.size(); i++) {
         for (int j = i + 1; j < nodes.size(); j++) {
-          Node antenna1 = nodes.get(i);
-          Node antenna2 = nodes.get(j);
+          final Node antenna1 = nodes.get(i);
+          final Node antenna2 = nodes.get(j);
           double slope =
               (double) (antenna1.row() - antenna2.row()) / (antenna1.col() - antenna2.col());
-          double intersect = (double) antenna1.row() - (slope * antenna1.col());
+          final double intersect = (double) antenna1.row() - (slope * antenna1.col());
           for (int row = 0; row < this.numRows; row++) {
             for (int col = 0; col < this.numCols; col++) {
-              double tolerance = 1e-9;
+              final double tolerance = 1e-9;
               if (Math.abs(row - (slope * col + intersect)) < tolerance) {
                 antiNodesPart.add(new Node(row, col));
               }
